7ffd9b97147a4af00ce917b97a54c68f964d16f7
kmatosx <152808444+kmatosx@users.noreply.github.com>
2023-12-05 16:08:10 -0500

    Update maze_solver_frontend.py
    -kathy comments inserted

--- maze_solver_frontend.py
+++ maze_solver_frontend.py
@@ -5,24 +5,28 @@ from PyQt6.QtGui import QPen, QColor
 from PyQt6.QtCore import Qt
 from maze_solver_backend import generate_maze_iterative, dijkstra, a_star
 
+#display window
 class GraphicsView(QGraphicsView):
     def __init__(self, scene):
         super().__init__(scene)
-
+        
+    #for viewing purposes of a dense maze
     def wheelEvent(self, event):
         factor = 1.1
         if event.angleDelta().y() > 0:
             self.scale(factor, factor)
         else:
             self.scale(1 / factor, 1 / factor)
-
+            
+#load the space to draw the maze
 class MazeSolverApp(QMainWindow):
     maze_w = 400
     maze_h = 250
     def __init__(self):
         super().__init__()
         self.initUI()
-
+        
+    #UI additatives, buttons and etc.
     def initUI(self):
         # Window setup
         self.setWindowTitle("Maze Solver")
@@ -63,39 +67,45 @@ class MazeSolverApp(QMainWindow):
         self.view.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAsNeeded)
         layout.addWidget(self.view)
 
+    #random generation of maze, selection of sizes for better presentation
     def generateMaze(self):
         cur_size = self.size_selection.currentIndex()
         if cur_size == 0:
+            #400X250 maze size
             self.maze_w = 400
             self.maze_h = 250
             self.view.resetTransform()
             self.view.scale(1, 1)
 
         if cur_size == 1:
+            #150X100 maze size
             self.maze_w = 150
             self.maze_h = 100
             self.view.resetTransform()
             self.view.scale(2.5, 2.5)
 
         if cur_size == 2:
+            #40X25 maze size
             self.maze_w = 40
             self.maze_h = 25
             self.view.resetTransform()
             self.view.scale(10, 10)
 
         if cur_size == 3:
+            #20X10 maze size
             self.maze_w = 20
             self.maze_h = 10
             self.view.resetTransform()
             self.view.scale(20, 20)
 
+        #after selection, generate and draw the layout of maze in backend
         self.maze = generate_maze_iterative(self.maze_w, self.maze_h)
         self.drawMaze()
         self.drawStartExit()
 
-
+    #drawing random empty maze, cells in stack from backend will be colored white
     def drawMaze(self):
-        cell_size = 2  # Size of each cell in the visualization
+        cell_size = 2  # size of each cell in the visualization
         self.scene.setSceneRect(0, 0, self.maze_w * cell_size, self.maze_h * cell_size)
         self.scene.clear()
         self.scene.addRect(-cell_size, -cell_size, (self.maze_w+2)*cell_size, (self.maze_h+2)*cell_size, QPen(Qt.GlobalColor.black), QColor(Qt.GlobalColor.black))
@@ -103,37 +113,37 @@ class MazeSolverApp(QMainWindow):
         for y in range(self.maze_h):
             for x in range(self.maze_w):
                 color = Qt.GlobalColor.white
-                if self.maze[y][x] == 1:  # Avoid drawing over the edge
+                if self.maze[y][x] == 1:  # avoid drawing over the edge
                     self.scene.addRect(x * cell_size, y * cell_size, cell_size-1, cell_size-1, QPen(color), QColor(color))
-
-
-
-
+    
+    #indicators of start (grey) and end (blue) cells
     def drawStartExit(self):
-        cell_size = 2  # Size of each cell in the visualization
+        cell_size = 2  # size of each cell in the visualization
 
-        # Mark the start
+        # mark the start
         start_x, start_y = (1), (0)  #
         exit_color = Qt.GlobalColor.gray  #
         self.scene.addRect(start_x * cell_size, start_y * cell_size, cell_size-1, cell_size-1, QPen(exit_color),
                            QColor(exit_color))
-        # Mark the exit
-        exit_x, exit_y = (self.maze_w-2), (self.maze_h-1)  # Adjust if your exit coordinates are different
-        exit_color = Qt.GlobalColor.blue  # Exit marked in blue
+        # mark the exit
+        exit_x, exit_y = (self.maze_w-2), (self.maze_h-1)  # adjust if your exit coordinates are different
+        exit_color = Qt.GlobalColor.blue  # exit marked in blue
         self.scene.addRect(exit_x * cell_size, exit_y * cell_size, cell_size-1, cell_size-1, QPen(exit_color),
                            QColor(exit_color))
+    #re-generating maze -> clear and re-initalize the maze generation
     def redrawMaze(self):
-        cell_size = 2  # Size of each cell in the visualization
-        self.scene.clear()  # Clear the scene first
+        cell_size = 2  # size of each cell in the visualization
+        self.scene.clear()  # clear the scene first
         for y in range(self.maze_h):
             for x in range(self.maze_w):
                 color = Qt.GlobalColor.white if self.maze[y][x] == 0 else Qt.GlobalColor.black
                 self.scene.addRect(x * cell_size, y * cell_size, cell_size, cell_size, QPen(color), QColor(color))
-
+    
+    # redefines start and end points after generation of maze and calls pathfinding algorithms
     def solveMaze(self):
-        self.drawMaze()  # Redraw the maze to clear previous paths
-        start_point = (1, 0)  # Entrance
-        end_point = (self.maze_w-2, self.maze_h-1)  # Exit
+        self.drawMaze()  # redraw the maze to clear previous paths
+        start_point = (1, 0)  # entrance
+        end_point = (self.maze_w-2, self.maze_h-1)  # exit
         selected_algorithm = self.algorithm_selection.currentText()
 
         if selected_algorithm == "Dijkstra's Algorithm":
@@ -145,7 +155,8 @@ class MazeSolverApp(QMainWindow):
         self.drawStartExit()
         print(selected_algorithm, "Number of steps:", len(self.all_steps))
         print(selected_algorithm, "Length of path:", len(self.path))
-
+        
+    # Given the path from maze_solver_backend, draw paths (green) and steps (red)
     def drawPath(self):
         cell_size = 2  # Adjust as needed
         all_steps_pen = QPen(QColor(255, 0, 0))  # Red color for all steps
